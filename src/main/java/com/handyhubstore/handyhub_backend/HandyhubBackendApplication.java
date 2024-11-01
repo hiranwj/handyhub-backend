1ce83b80bc050e1ad64fc3ee43075cf24bd546c7
hiran_xdoto <hiran@xdoto.io>
2024-11-01 11:17:56 +0530

    Initial commit

---
+++ src/main/java/com/handyhubstore/handyhub_backend/HandyhubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.handyhubstore.handyhub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandyhubBackendApplication.class, args);
+	}
+
+}
